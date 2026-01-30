3952aeff0c6ab87315ed42f0895880b7ca73df2d
igctreinamentoscorporativo <igctreinamentoscorporativo@gmail.com>
2026-01-30 19:42:13 -0300

    Add files via upload

---
+++ js/igc-config.js
@@ -0,0 +1,19 @@
+// js/igc-config.js
+
+window.IGC_CONFIG = {
+  APPS_SCRIPT_BASE:
+    "https://script.google.com/macros/s/AKfycbxB6mH2EqLyjQaUtMPMyx-GNLzHColazB0awBetRDG_Un3lCvYaF0tHRATIWOa7Iu6HsQ/exec",
+
+  APPS: {
+    ADMIN: "admin",
+    TOKEN: "token",
+    LOGIN: "login"
+  }
+};
+
+// 🔹 Helper global (opcional, mas profissional)
+window.IGC = {
+  app(name) {
+    return `${window.IGC_CONFIG.APPS_SCRIPT_BASE}?app=${window.IGC_CONFIG.APPS[name]}`;
+  }
+};
