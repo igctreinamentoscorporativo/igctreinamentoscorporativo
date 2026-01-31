ac93bd2495f46da7ec357737fbe71a30c627fe3a
igctreinamentoscorporativo <igctreinamentoscorporativo@gmail.com>
2026-01-31 16:21:12 -0300

    Update igc-config.js

--- js/igc-config.js
+++ js/igc-config.js
@@ -2,7 +2,7 @@
 
 window.IGC_CONFIG = {
   APPS_SCRIPT_BASE:
-    "https://script.google.com/macros/s/AKfycbxwHIRcG2PnZKxt49mGMfbO84Nps_Hju6YD2IqUBXU_jldQ6mmEeHG5MmHlNJdlV_lUgQ/exec",
+    "https://script.google.com/macros/s/AKfycbw2VCh3hgm5FBafeWnaS5TrF0kkNnhOod_eZwiY0Lb08jjps3T4R-ostAVaq_KJBTKSMw/exec",
 
   APPS: {
     ADMIN: "admin",
