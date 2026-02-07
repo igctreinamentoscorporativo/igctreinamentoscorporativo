cdcdee6404c9ad5050a00cb8021adb94b54c3f52
igctreinamentoscorporativo <igctreinamentoscorporativo@gmail.com>
2026-02-06 23:46:35 -0300

    Update igc-config.js

--- js/igc-config.js
+++ js/igc-config.js
@@ -1,12 +1,8 @@
 // js/igc-config.js
+
 window.IGC_CONFIG = {
-  // 🔹 BASE PRINCIPAL (HTML / navegação / apps)
   APPS_SCRIPT_BASE:
     "https://script.google.com/macros/s/AKfycby5QMCXGXIJXlRqN-k31YY4Vlv4K4I4aFKmfGeaFp30uu8ohFiwFU76o5LhHcj3FwExUA/exec",
-
-  // 🔹 BASE EXCLUSIVA PARA FETCH / JSON (CORS OK)
-  APPS_SCRIPT_API:
-    "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLg6PXnRNYBKx7snVoCVDLkympEltXzX3CXQd68hcpCWs6njTX73f1Xp8FWSKpTqHT_LqAFRJPQqfnQ-F_AhbALLWU8eA0RhPyG8hiTw1-bE9STYMBHW4MkuOHNXiC0zOazNVMWDo2UDxBNcLDCgd1PcsgzclGGbexE8QqyoPOn_cGx1uoKOREouqVRdSIF8a5oSZ-C-jy_6aeACMPMnANSmcCtRpLT7UGgvi5zK4rc8aeEC9RvVv8UqLM9xWeMv60A4iyJz_MEMCMnRY9JbOozSanfzSZ-m5jnDrB6J&lib=ML1zOo_OUUflxdLMuXK8vDNaDE_gWchIX",
 
   APPS: {
     ADMIN: "admin",
@@ -15,17 +11,9 @@ window.IGC_CONFIG = {
   }
 };
 
-// helpers
+// 🔹 Helper global (opcional, mas profissional)
 window.IGC = {
   app(name) {
     return `${window.IGC_CONFIG.APPS_SCRIPT_BASE}?app=${window.IGC_CONFIG.APPS[name]}`;
-  },
-
-  api(params = {}) {
-    const url = new URL(window.IGC_CONFIG.APPS_SCRIPT_API);
-    Object.entries(params).forEach(([k, v]) => {
-      url.searchParams.append(k, v);
-    });
-    return url.toString();
   }
 };
