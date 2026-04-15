f0587349f82619ceb2606fc09c1edd2c93ac2cc2
igctreinamentoscorporativo <igctreinamentoscorporativo@gmail.com>
2026-04-15 15:31:03 -0300

    Update produto.js

--- js/produto.js
+++ js/produto.js
@@ -87,19 +87,6 @@ function montarProduto(produto) {
 `;
 
 
-// 🔥🔥🔥 AGORA COLE ISSO EXATAMENTE AQUI 🔥🔥🔥
-
-  // limpa fundo anterior (IMPORTANTE)
-  document.body.style.background = "";
-  document.body.classList.remove("produto-fundo-custom");
-
-  // aplica fundo se existir no produto
-  if (produto.configuracoes?.fundoPagina) {
-    document.body.style.background = produto.configuracoes.fundoPagina;
-    document.body.classList.add("produto-fundo-custom");
-  }
-
-  
 
   // ----- DESCRIÇÃO COMPLETA -----
   if (produto.descricao?.accordion) {
