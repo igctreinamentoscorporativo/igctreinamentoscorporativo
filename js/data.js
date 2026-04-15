87f296c36579a7dea09e1c41fa30dd3780434d31
igctreinamentoscorporativo <igctreinamentoscorporativo@gmail.com>
2026-04-15 00:38:30 -0300

    Update image path for Maringá event

--- js/data.js
+++ js/data.js
@@ -467,7 +467,7 @@ textoParcelamento: "<strong> Fale com um consultor agora. Garanta sua vaga na pr
     nome: "Você sabe o que precisa fazer…<br>mas não consegue executar?<br><span style='font-size:0.9em;font-weight:normal'>Imersão Practitioner em PNL – Maringá</span>",
     categoria: "pnl",
     preco: 1997.00,
-    imagem: "assets/img/maringaarte.png",
+    imagem: "assets/img/PNLMARINGA01.jpeg",
     videos: [
       "assets/videos/PNLIMERSAO.mp4"
     ],
