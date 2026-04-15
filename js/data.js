6fd04c1f16c5dd7606ce6b8c66a55eb96fe75d7e
igctreinamentoscorporativo <igctreinamentoscorporativo@gmail.com>
2026-04-15 13:36:14 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -827,7 +827,7 @@ Sim.
 
 
  configuracoes: {
-  corFundo: "linear-gradient(rgba(255,255,255,0.65), rgba(255,255,255,0.65)) padding-box,",   
+
   mostrarNome: true,
   mostrarPreco: false,
   mostrarResumo: true,
