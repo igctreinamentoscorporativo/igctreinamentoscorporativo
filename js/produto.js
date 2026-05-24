34017d7a8bd829924ce9a9d59dac2bd98c6c517d
igctreinamentoscorporativo <igctreinamentoscorporativo@gmail.com>
2026-05-24 20:39:47 -0300

    Modify participation offer text in produto.js
    Updated promotional text for participation offer.

--- js/produto.js
+++ js/produto.js
@@ -167,7 +167,7 @@ if (produto.configuracoes?.cta?.mostrar) {
 
 -->
     <span class="cta-beneficio">
-      🎁 Participe e concorra a 50% OFF + levar 1 convidado
+      🎁 Participe e concorra
     </span>
       
     </div>
